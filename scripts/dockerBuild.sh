7d0bd9db0d1c70ec712e947f4a7d6999a9d1a8a1
furkle <furkle@furkleindustries.com>
2018-06-22 23:03:32 -0400

    Adding echo to end of dockerRun script.

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t furkleindustries-homepage /etc/furkleindustries-homepage
+docker build -t furkleindustries-homepage /etc/furkleindustries-homepage && echo 'dockerBuild task complete.'
