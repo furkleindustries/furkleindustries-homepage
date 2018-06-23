f6da4ac3bf6a202282ec9cb0c53e6f9225fa3198
furkle <furkle@furkleindustries.com>
2018-06-23 00:41:26 -0400

    Fixing typo in dockerRebuild.sh.

--- scripts/dockerRebuild.sh
+++ scripts/dockerRebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/etc/furkleindustries-homepage/scripts/dockerKill.sh && /etc/furkleindustries-homepage/scripts/dockerClean.sh && /etc/furkleindustries-homepage/dockerBuild.sh && /etc/furkleindustries-homepage/scripts/dockerRun.sh && echo 'dockerRebuild task complete.'
+/etc/furkleindustries-homepage/scripts/dockerKill.sh && /etc/furkleindustries-homepage/scripts/dockerClean.sh && /etc/furkleindustries-homepage/scripts/dockerBuild.sh && /etc/furkleindustries-homepage/scripts/dockerRun.sh && echo 'dockerRebuild task complete.'
