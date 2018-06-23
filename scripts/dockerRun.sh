54167e2bc73149e9dad2208034003f2577448f63
furkle <furkle@furkleindustries.com>
2018-06-22 22:45:04 -0400

    Updating dockerRun.sh.

--- scripts/dockerRun.sh
+++ scripts/dockerRun.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-if "$h2" == "h2"; then
-  docker run -d --name furkleindustries-homepage -p 80:3001 -p 443:3000 -v /etc/letsencrypt/live/furkleindustries.com/:/etc/furkleindustries-homepage/secrets/ furkleindustries-homepage
-else
-  docker run -d --name furkleindustries-homepage -p 80:3000 -v /etc/letsencrypt/live/furkleindustries.com/:/etc/furkleindustries-homepage/secrets/ furkleindustries-homepage
-fi
-
-echo 'dockerRun task complete.'
+docker run -d --name furkleindustries-homepage -p 80:3001 -p 443:3000 -v /etc/letsencrypt/live/furkleindustries.com/:/etc/furkleindustries-homepage/secrets/ furkleindustries-homepage && echo 'dockerRun task complete.'
