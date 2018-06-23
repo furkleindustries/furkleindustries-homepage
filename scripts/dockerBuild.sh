c3c2bd947f355af2f16021959ab59ea972631cca
furkle <furkle@furkleindustries.com>
2018-06-23 13:27:18 -0400

    Forgot to add mkdir -p for secrets/ssl.

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-cp /etc/letsencrypt/live/furkleindustries.com/privkey.pem /etc/furkleindustries-homepage/secrets/ &&
-cp /etc/letsencrypt/live/furkleindustries.com/fullchain.pem /etc/furkleindustries-homepage/secrets/ &&
+mkdir -p /etc/furkleindustries-homepage/secrets/ssl/ &&
+cp /etc/letsencrypt/live/furkleindustries.com/privkey.pem /etc/furkleindustries-homepage/secrets/ssl/ &&
+cp /etc/letsencrypt/live/furkleindustries.com/fullchain.pem /etc/furkleindustries-homepage/secrets/ssl/ &&
 docker build -t furkleindustries-homepage /etc/furkleindustries-homepage && echo 'dockerBuild task complete.'
