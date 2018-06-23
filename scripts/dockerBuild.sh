f3a61cde501104fb5d1d630e6247e8ec78691b48
furkle <furkle@furkleindustries.com>
2018-06-23 13:26:18 -0400

    Accidentally put SSL key copying in dockerRun rather than dockerBuild.

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+cp /etc/letsencrypt/live/furkleindustries.com/privkey.pem /etc/furkleindustries-homepage/secrets/ &&
+cp /etc/letsencrypt/live/furkleindustries.com/fullchain.pem /etc/furkleindustries-homepage/secrets/ &&
 docker build -t furkleindustries-homepage /etc/furkleindustries-homepage && echo 'dockerBuild task complete.'
--- scripts/dockerRun.sh
+++ scripts/dockerRun.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-cp /etc/letsencrypt/live/furkleindustries.com/privkey.pem /etc/furkleindustries-homepage/secrets/ &&
-cp /etc/letsencrypt/live/furkleindustries.com/fullchain.pem /etc/furkleindustries-homepage/secrets/ &&
 docker run -d --name furkleindustries-homepage -p 80:3001 -p 443:3000 furkleindustries-homepage &&
 echo 'dockerRun task complete.'
