a6151249107880df88edab6d524c1d7bc2c1b075
furkle <furkle@furkleindustries.com>
2019-05-24 12:58:17 -0400

    Updating ink-interactive-tutorial.

---
+++ src/assets/fictions/ink/ink_resources/ink-interactive-tutorial/precache-manifest.935a76d52fd29c449ed77e206859d113.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0dc0dfaa2f69693b596300dd9e0b2ca",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/index.html"
+  },
+  {
+    "revision": "4463e3d017703ea57614",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/css/3.6829684c.chunk.css"
+  },
+  {
+    "revision": "3dc5a606cbdeabeb4cbe",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/css/main.228116a8.chunk.css"
+  },
+  {
+    "revision": "ddd8d8747537aadd5bed",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/10.ba6a384d.chunk.js"
+  },
+  {
+    "revision": "096e7f5dcfb68bbf842a",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/11.174d3065.chunk.js"
+  },
+  {
+    "revision": "b3db6f98f10af77142ac",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/12.7822e5ac.chunk.js"
+  },
+  {
+    "revision": "9fa5d67e239f3b210b2f",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/13.275c220d.chunk.js"
+  },
+  {
+    "revision": "10e9e1eb57df68d4e464",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/14.5a665889.chunk.js"
+  },
+  {
+    "revision": "390a6cde11b6e14b55de",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/15.03472b62.chunk.js"
+  },
+  {
+    "revision": "4a5fa54ca6b4b3b6c50f",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/16.228c2afa.chunk.js"
+  },
+  {
+    "revision": "021faf363cd9ac916dbc",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/17.e5100f02.chunk.js"
+  },
+  {
+    "revision": "822f393873e28bb13c99",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/18.1c584024.chunk.js"
+  },
+  {
+    "revision": "0cb315899779e6ef1e1c",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/19.4c226ae7.chunk.js"
+  },
+  {
+    "revision": "ba40a9734a0dd449e80f",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/2.641334ce.chunk.js"
+  },
+  {
+    "revision": "a72e31084dc74a81978c",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/20.adfd5f98.chunk.js"
+  },
+  {
+    "revision": "dbe9ab08dae422b28cfe",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/21.d70df325.chunk.js"
+  },
+  {
+    "revision": "a76aea00f42d7a94d7a8",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/22.5d6c393c.chunk.js"
+  },
+  {
+    "revision": "838c3d59e279ad1df98b",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/23.63929035.chunk.js"
+  },
+  {
+    "revision": "07070709fd411504e257",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/24.6b2803b8.chunk.js"
+  },
+  {
+    "revision": "8962b7f379a61224a107",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/25.5fec96db.chunk.js"
+  },
+  {
+    "revision": "d3f393bbe10509f61334",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/26.fb325809.chunk.js"
+  },
+  {
+    "revision": "7d96b7d7d232eac8d682",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/27.2c7bffd4.chunk.js"
+  },
+  {
+    "revision": "0435f39a8bee28ac14ff",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/28.3db1859b.chunk.js"
+  },
+  {
+    "revision": "3c9b02bf2e10f2bff83c",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/29.5e19dd68.chunk.js"
+  },
+  {
+    "revision": "4463e3d017703ea57614",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/3.39767ada.chunk.js"
+  },
+  {
+    "revision": "f3769f83c3499f898dad",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/30.8d115be9.chunk.js"
+  },
+  {
+    "revision": "6997a0c7ab6f14997cce",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/31.f774d68a.chunk.js"
+  },
+  {
+    "revision": "d417a6ef1d7437114867",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/32.b859526c.chunk.js"
+  },
+  {
+    "revision": "d778dec01fb5f886bfb5",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/33.9f11f8ed.chunk.js"
+  },
+  {
+    "revision": "16fc6eb891b77b05ba55",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/34.62ce0c60.chunk.js"
+  },
+  {
+    "revision": "98ada464c8c73e77a297",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/35.0584fda0.chunk.js"
+  },
+  {
+    "revision": "ec8db2c7667048d41b23",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/36.84abb21f.chunk.js"
+  },
+  {
+    "revision": "8e2d76ec1ce03fae8d58",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/37.043cfab9.chunk.js"
+  },
+  {
+    "revision": "d7e8bac3f0cc8ff8f1fb",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/38.1b4c0cba.chunk.js"
+  },
+  {
+    "revision": "fc978dd74dc2b2c8d627",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/39.e6ffc4cc.chunk.js"
+  },
+  {
+    "revision": "54213a2d0f01a26a2900",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/4.50c3a01f.chunk.js"
+  },
+  {
+    "revision": "96872d0a7ad500ac1c27",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/40.c3ad3e0b.chunk.js"
+  },
+  {
+    "revision": "756d3bd2608f51d9cab9",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/41.aad14809.chunk.js"
+  },
+  {
+    "revision": "c80dbf9f71a1f2d783ba",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/42.09b55ac8.chunk.js"
+  },
+  {
+    "revision": "c8769648f7f768860eed",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/43.cbe94b91.chunk.js"
+  },
+  {
+    "revision": "f83fd58e84e2a5df11ca",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/5.25e92d83.chunk.js"
+  },
+  {
+    "revision": "b5e3e36f0ec66d29bd9f",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/6.803446e2.chunk.js"
+  },
+  {
+    "revision": "7fd595c9637998ad7596",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/7.e0350530.chunk.js"
+  },
+  {
+    "revision": "ba0087f87245f9d7da64",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/8.94d28a77.chunk.js"
+  },
+  {
+    "revision": "3859c69787e46feee31f",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/9.eec13a43.chunk.js"
+  },
+  {
+    "revision": "3dc5a606cbdeabeb4cbe",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/main.b8181980.chunk.js"
+  },
+  {
+    "revision": "71057d3bf4fae949b860",
+    "url": "/fictions/ink/ink_resources/ink-interactive-tutorial/static/js/runtime~main.bfa0c90b.js"
+  }
+]);
